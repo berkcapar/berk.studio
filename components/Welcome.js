5dff114c6b9ece9924b4d32f71978afa695462d9
Berk Çapar <berkcapar@gmail.com>
2020-11-13 00:36:56 +0300

    waving hand

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 
 const Welcome = () => {
   return (
     <div className="welcomecontainer">
       <div className="firstpar">
         <h2>Hello</h2>
-        <img src="/wavinghand.png" alt="wavinghand"></img>
+        <span className="wave">👋</span>
       </div>
       <div className="subcontainer">
         <h2>I'm Berk Çapar</h2>
@@ -16,75 +16,109 @@ const Welcome = () => {
         <p>Technical Project Manager and Freelance Web Developer.</p>
         <p>Based in Istanbul.</p>
       </div>
-      <nav> 
-          <Link href="/who"> 
-          <a>More about me <i className="fas fa-long-arrow-alt-right"></i></a> 
-          </Link>
-          <Link href="/now">
-          <a>What am I doing now <i className="fas fa-long-arrow-alt-right"></i></a>
-          </Link>
-          </nav>
+      <nav>
+        <Link href="/who">
+          <a>
+            More about me <i className="fas fa-long-arrow-alt-right"></i>
+          </a>
+        </Link>
+        <Link href="/now">
+          <a>
+            What am I doing now <i className="fas fa-long-arrow-alt-right"></i>
+          </a>
+        </Link>
+      </nav>
       <div className="externalcontainer">
-         <a href="https://www.linkedin.com/in/berkcapar/">Linkedin</a>
-          <a href="https://github.com/berkcapar">Github</a>
+        <a href="https://www.linkedin.com/in/berkcapar/">Linkedin</a>
+        <a href="https://github.com/berkcapar">Github</a>
       </div>
       <style jsx>
         {`
-        nav{
+          nav {
             color: lightsalmon;
-            margin-top: 0.5rem;    
+            margin-top: 0.5rem;
             padding-bottom: 2rem;
             margin-left: 1rem;
             width: 60%;
             display: flex;
-            flex-wrap: wrap;  
-        }
-        nav a {
+            flex-wrap: wrap;
+          }
+          nav a {
             margin-right: 1rem;
             margin-bottom: 0.5rem;
-        }
+          }
           .firstpar {
             display: flex;
             margin-left: 1rem;
           }
-          img {
-            max-width: 50px;
-            max-height: 50px;
-            margin-left: 1rem;
-            margin-top: 0.5rem;
+          .wave {
+            animation-name: wave-animation; 
+            animation-duration: 2.5s; 
+            animation-iteration-count: infinite; 
+            transform-origin: 70% 70%; 
+            display: inline-block;
+            margin-left: 0.5rem;
+            padding-top: 0.4rem;
+          
+          }
+
+          @keyframes wave-animation {
+            0% {
+              transform: rotate(0deg);
+            }
+            10% {
+              transform: rotate(14deg);
+            } 
+            20% {
+              transform: rotate(-8deg);
+            }
+            30% {
+              transform: rotate(14deg);
+            }
+            40% {
+              transform: rotate(-4deg);
+            }
+            50% {
+              transform: rotate(10deg);
+            }
+            60% {
+              transform: rotate(0deg);
+            } 
+            100% {
+              transform: rotate(0deg);
+            }
           }
           .welcomecontainer {
             display: flex;
             flex-direction: column;
             margin-top: 1rem;
             margin-left: 0.7rem;
-            width: 90%
+            width: 90%;
           }
           .subcontainer {
             margin-left: 1rem;
           }
-          .externalcontainer{
-            color: white;  
+          .externalcontainer {
+            color: white;
             display: flex;
             justify-content: center;
           }
           .externalcontainer a {
-              border: 1px solid white;
-              border-radius: 1.5rem;
-              margin-right: 1rem;
-              padding: 1rem;       
+            border: 1px solid white;
+            border-radius: 1.5rem;
+            margin-right: 1rem;
+            padding: 1rem;
           }
           i {
             color: lightsalmon;
             margin-left: 0.125rem;
             padding-top: 0.125rem;
           }
-          @media (min-width:675px){
-            .welcomecontainer{
+          @media (min-width: 675px) {
+            .welcomecontainer {
               width: 50%;
               margin: 1rem auto;
             }
-            
           }
         `}
       </style>
