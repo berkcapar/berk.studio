10092ffd3671765df8ebe58abfcdb85db44b6b52
Berk Çapar <berkcapar@gmail.com>
2020-11-13 16:23:59 +0300

    update header

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="sitetitle">
       <Link href="/">
         <a>
-          <h1>berk.studio</h1>
+          <h1>berk çapar</h1>
         </a>
       </Link>
       <style jsx>{`
