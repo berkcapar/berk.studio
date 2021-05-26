1e7a898443e9b0a8c03c3c8ae91e48a4ebcf4657
Berk Çapar <berkcapar@gmail.com>
2021-05-26 10:20:28 +0300

    projects completed

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import ProjectsNav from "../components/ProjectsNav";
+import Link from "next/link";
 
 const Projects = () => {
   return (
@@ -47,23 +48,71 @@ const Projects = () => {
           </div>
         </div>
       </div>
+      <div className="developer-roles-container">
+        <div className="developer-roles">
+          <a href="https://www.denizerden.com">
+            <img href="" alt="Deniz Erden" src="./denizerden.png" />
+          </a>
+          <div className="developer-roles-text">
+            <h3>denizerden.com</h3>
+            <p>Musician Portfolio builded with Next.js</p>
+          </div>
+        </div>
+        <div className="developer-roles">
+          <a href="https://www.ergunbaydi.com">
+            <img alt="Ergün Baydı" src="./ergunbaydi.png" />
+          </a>
+          <div className="product-manager-roles-text">
+            <h3>ergunbaydi.com</h3>
+            <p>Photographer Portfolio builded with Next.js</p>
+          </div>
+        </div>
+        <div className="developer-roles">
+          <a href="https://www.ergunbaydi.com">
+            <img alt="React Project" src="./movieapp.png" />
+          </a>
+          <div className="product-manager-roles-text">
+            <h3>Movie Memory</h3>
+            <p>
+              Movie Search from IMDB API. builded with MERN stack including
+              login, search, add to fav features.
+            </p>
+          </div>
+        </div>
+      </div>
+
       <style jsx>{`
-        .product-manager-roles-container {
+        .product-manager-roles-container,
+        .developer-roles-container {
           width: 50%;
           margin: 0 auto;
         }
-        .product-manager-roles {
+        .product-manager-roles,
+        .developer-roles {
           display: flex;
           justify-content: space-around;
-          margin-bottom: 1rem;
+          padding-bottom: 1rem;
+          margin-top: 1rem;
           border-bottom: 1px solid white;
         }
+        .developer-roles-container {
+          padding-top: 5rem;
+        }
         .product-manager-roles img {
-          width: 40%;
           max-width: 200px;
+          width: 40%;
+        }
+        .developer-roles img {
+          width: 100%;
+        }
+        .developer-roles a {
+          width: 60%;
         }
         .product-manager-roles div {
           width: 50%;
+        }
+        .developer-roles div {
+          width: 30%;
         }
       `}</style>
     </div>
